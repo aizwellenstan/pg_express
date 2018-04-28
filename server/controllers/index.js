374053d80c630f8260f76b7fec176993ac5eb20e
David de Moura Marques <mouradavid8@gmail.com>
2018-04-28 17:00:22 -0300

    Add files via upload

---
+++ server/controllers/index.js
@@ -0,0 +1,8 @@
+const todos = require("./todo");
+//Não deixa explicito no tutorial a adição da linha abaixo
+const todoItems = require("./todoitems");
+
+module.exports = {
+  todos,
+  todoItems
+};
